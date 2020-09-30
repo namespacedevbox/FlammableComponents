3e41b02e05874ba3b492249352d19d61c60e8301
CORE <namespacedevbox@gmail.com>
2020-09-30 12:52:22 +0300

    Fixed DateTimePicker

--- FlammableComponents/Components/DateTimePicker/DateTimePicker.razor.cs
+++ FlammableComponents/Components/DateTimePicker/DateTimePicker.razor.cs
@@ -1,21 +1,72 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System;
+using System.Collections.Generic;
 
 namespace FlammableComponents
 {
-    public partial class DateTimePicker : ComponentBase
+    public partial class DateTimePicker<TValue> : ComponentBase
     {
-        [Parameter] public DateTime? Value { get; set; }
-        [Parameter] public EventCallback<DateTime?> ValueChanged { get; set; }
+        private const string DateMax = "9999-12-31T23:59";
+
+        [Parameter(CaptureUnmatchedValues = true)] public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+        [Parameter] public TValue Value { get; set; }
+        [Parameter] public EventCallback<TValue> ValueChanged { get; set; }
 
         private void OnValueChanged(ChangeEventArgs args)
         {
-            var datetime = args.Value.ToString();
+            var value = args.Value.ToString();
 
-            if (string.IsNullOrEmpty(datetime))
+            if (string.IsNullOrEmpty(value))
                 return;
 
-            ValueChanged.InvokeAsync(DateTime.Parse(datetime));
+            TValue result;
+
+            var targetType = Nullable.GetUnderlyingType(typeof(TValue)) ?? typeof(TValue);
+
+            if (targetType == typeof(DateTime))
+            {
+                TryParseDateTime(value, out result);
+                ValueChanged.InvokeAsync(result);
+            }
+            else if (targetType == typeof(DateTimeOffset))
+            {
+                TryParseDateTimeOffset(value, out result);
+                ValueChanged.InvokeAsync(result);
+            }
+            else
+            {
+                throw new InvalidOperationException($"The type '{targetType}' is not a supported date type.");
+            }
+        }
+
+        private bool TryParseDateTime(string value, out TValue result)
+        {
+            var success = DateTime.TryParse(value, out var parsedValue);
+            if (success)
+            {
+                result = (TValue)(object)parsedValue;
+                return true;
+            }
+            else
+            {
+                result = default;
+                return false;
+            }
+        }
+
+        private bool TryParseDateTimeOffset(string value, out TValue result)
+        {
+            var success = DateTimeOffset.TryParse(value, out var parsedValue);
+            if (success)
+            {
+                result = (TValue)(object)parsedValue;
+                return true;
+            }
+            else
+            {
+                result = default;
+                return false;
+            }
         }
     }
 }
